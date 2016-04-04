c6dfb14459b81692da1d46aa20cde564aabf33cd
jsirera <judsirera@gmail.com>
2016-04-04 16:38:39 +0200

    g4 practica

--- configure/dispatcher.config.php
+++ configure/dispatcher.config.php
@@ -5,19 +5,4 @@
 
 $config['default']				= 'HomeHomeController';
 $config['home']					= 'HomeHomeController';
-$config['practica1']			= 'PagesPractica1Controller';
-$config['practica2']			= 'PagesPractica2Controller';
-$config['formulari']			= 'PagesFormulariController';
-$config['galeria']			    = 'PagesGaleriaController';
-$config['vent']			        = 'PagesVentController';
-$config['corda']			    = 'PagesCordaController';
-$config['percussio']			= 'PagesPercussioController';
-
-$config['borrar']	            = 'PagesBorrarController';
-
-
-$config['practica3']			= 'PagesPractica3Controller';
-$config['p3_galeria']			= 'PagesPractica3_galeriaController';
-
-$config['practica4']			= 'PagesPractica4Controller';
-$config['edit']			    = 'PagesEditarController';
+$config['prova']				= 'HomeProvaController';
